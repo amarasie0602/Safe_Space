b33f0d97faecfb00b592988e1b15669edd799520
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-04 12:00:36 +0530

    feat: implement register - hash password with bcryptjs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,13 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.status(501).json({ message: 'Not implemented' });
+  const { pseudonym, password } = req.body;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await User.create({ pseudonym, passwordHash });
+
+  res.status(201).json({ user: { id: user._id, pseudonym: user.pseudonym, role: user.role } });
 };
 
 const login = async (req, res) => {
