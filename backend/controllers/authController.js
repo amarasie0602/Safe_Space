45ff04e46381d91308602b797ff9d0146b68bb20
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-04 12:00:49 +0530

    feat: check pseudonym uniqueness before saving on register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,11 @@ const User = require('../models/User');
 const register = async (req, res) => {
   const { pseudonym, password } = req.body;
 
+  const existing = await User.findOne({ pseudonym });
+  if (existing) {
+    return res.status(409).json({ message: 'Pseudonym already taken' });
+  }
+
   const passwordHash = await bcrypt.hash(password, 10);
   const user = await User.create({ pseudonym, passwordHash });
 
