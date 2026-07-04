5e19ced461fccca7e112a7152748780f531a11af
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-04 12:11:39 +0530

    feat: add GET /posts public route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { createPost } = require('../controllers/postController');
+const { createPost, getPosts } = require('../controllers/postController');
 
 const router = express.Router();
 
 router.post('/posts', protect, createPost);
+router.get('/posts', getPosts);
 
 module.exports = router;
