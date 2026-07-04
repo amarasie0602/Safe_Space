ddd2e8e353398b12005c09326815ac324dbbd25f
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-04 12:03:15 +0530

    feat: mount authRoutes in server.js under /auth

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
