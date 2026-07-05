be837f799567bc5850163fd7cbcd976c8b70e024
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:22:13 +0530

    feat: scaffold threadController

---
+++ backend/controllers/threadController.js
@@ -0,0 +1,9 @@
+const createThread = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+const getThreads = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+module.exports = { createThread, getThreads };
