28d23765066e0a9a1bc70fe469ecbaf214290962
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:25:37 +0530

    feat: add GET /threads public route

--- backend/routes/threadRoutes.js
+++ backend/routes/threadRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { createThread } = require('../controllers/threadController');
+const { createThread, getThreads, getThread } = require('../controllers/threadController');
 
 const router = express.Router();
 
 router.post('/threads', protect, createThread);
+router.get('/threads', getThreads);
+router.get('/threads/:id', getThread);
 
 module.exports = router;
