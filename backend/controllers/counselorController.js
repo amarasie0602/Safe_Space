be6209bfef92acfc808864b0abd6607f356b8798
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:29:04 +0530

    feat: scaffold counselorController

---
+++ backend/controllers/counselorController.js
@@ -0,0 +1,9 @@
+const register = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+const login = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+module.exports = { register, login };
