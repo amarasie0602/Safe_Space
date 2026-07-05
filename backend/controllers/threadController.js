f8097191b0252093cc04fa42e97ab26abb67de5b
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:23:40 +0530

    feat: add createThread handler

--- backend/controllers/threadController.js
+++ backend/controllers/threadController.js
@@ -1,5 +1,17 @@
+const Thread = require('../models/Thread');
+
 const createThread = async (req, res) => {
-  res.status(501).json({ message: 'Not implemented' });
+  const { category, title, body } = req.body;
+
+  const thread = await Thread.create({
+    author: req.user.id,
+    category,
+    title,
+    body,
+  });
+
+  await thread.populate('author', 'pseudonym');
+  res.status(201).json(thread);
 };
 
 const getThreads = async (req, res) => {
