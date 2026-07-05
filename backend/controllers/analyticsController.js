03e315d4152b84624d3b1691fd8aadedc9b576b6
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:41:43 +0530

    feat: add avg report resolution time to analytics pipeline

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post');
+const Report = require('../models/Report');
 
 const getAnalytics = async (req, res) => {
   const postsByCategory = await Post.aggregate([
@@ -7,7 +8,17 @@ const getAnalytics = async (req, res) => {
 
   const flaggedCount = await Post.countDocuments({ flagged: true });
 
-  res.json({ postsByCategory, flaggedCount });
+  const resolutionStats = await Report.aggregate([
+    { $match: { status: 'resolved', resolvedAt: { $ne: null } } },
+    { $project: { resolutionMs: { $subtract: ['$resolvedAt', '$createdAt'] } } },
+    { $group: { _id: null, avgResolutionMs: { $avg: '$resolutionMs' } } },
+  ]);
+
+  res.json({
+    postsByCategory,
+    flaggedCount,
+    avgResolutionMs: resolutionStats[0]?.avgResolutionMs || 0,
+  });
 };
 
 module.exports = { getAnalytics };
