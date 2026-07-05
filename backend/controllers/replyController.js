d6ac523a3930708aa5dfeb1adedc3b746d58595b
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-05 23:26:19 +0530

    feat: scaffold replyController

---
+++ backend/controllers/replyController.js
@@ -0,0 +1,9 @@
+const createReply = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+const flagReply = async (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+module.exports = { createReply, flagReply };
