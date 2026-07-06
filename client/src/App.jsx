e7b31b09e030f3b1c581885691a331b5fe5c8eaf
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-06 19:10:02 +0530

    feat: add always-visible crisis helpline banner in layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import PostFeed from './pages/PostFeed';
 import CreatePost from './pages/CreatePost';
 import ThreadList from './pages/ThreadList';
@@ -11,30 +12,32 @@ import PrivateRoute from './context/PrivateRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PostFeed />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/posts/new"
-        element={
-          <PrivateRoute>
-            <CreatePost />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/threads" element={<ThreadList />} />
-      <Route
-        path="/threads/new"
-        element={
-          <PrivateRoute>
-            <CreateThread />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/threads/:id" element={<ThreadDetail />} />
-      <Route path="/counselors" element={<CounselorList />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<PostFeed />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/posts/new"
+          element={
+            <PrivateRoute>
+              <CreatePost />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/threads" element={<ThreadList />} />
+        <Route
+          path="/threads/new"
+          element={
+            <PrivateRoute>
+              <CreateThread />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/threads/:id" element={<ThreadDetail />} />
+        <Route path="/counselors" element={<CounselorList />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,8 @@
+const Layout = ({ children }) => (
+  <div>
+    <div role="alert">If you are in crisis, call 988 (US) or your local emergency number now.</div>
+    <main>{children}</main>
+  </div>
+);
+
+export default Layout;
