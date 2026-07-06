04cb4e11cf779625804cd2f3a65488ff7d52b853
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-07-06 18:57:40 +0530

    feat: add ThreadList page with category filter

---
+++ client/src/pages/ThreadList.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../api/axios';
+
+const CATEGORIES = ['mental_health', 'family', 'financial', 'academic', 'relationships', 'addiction'];
+
+const ThreadList = () => {
+  const [threads, setThreads] = useState([]);
+  const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    const fetchThreads = async () => {
+      const { data } = await api.get('/threads', { params: category ? { category } : {} });
+      setThreads(data);
+    };
+    fetchThreads();
+  }, [category]);
+
+  return (
+    <div>
+      <h1>Threads</h1>
+      <div>
+        <button onClick={() => setCategory('')}>All</button>
+        {CATEGORIES.map((c) => (
+          <button key={c} onClick={() => setCategory(c)}>
+            {c}
+          </button>
+        ))}
+      </div>
+      {threads.map((thread) => (
+        <div key={thread._id}>
+          <Link to={`/threads/${thread._id}`}>{thread.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ThreadList;
